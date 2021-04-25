fa9a0062adfb75eef4d68bc35b98bbea9919dedb
dressaco <>
2021-04-25 04:17:49 +0000

    create if1

---
+++ 03-if1.py
@@ -0,0 +1,8 @@
+name = input('Digite o nome: ')
+age = int(input('Digite a idade: '))
+
+if age >= 65:
+  print('O paciente ' + name + ' possui atendimento prioritário!')
+else:
+  print('O paciente ' + name + 'NÃO possui atendimento prioritário!')
+print('FIM')
