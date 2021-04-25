74fa7e6b3929b8bf9969afe4a083b9f28ff459e8
dressaco <>
2021-04-25 03:38:49 +0000

    create var2

---
+++ 02-var2.py
@@ -0,0 +1,6 @@
+person_in_charge = input('Digite o nome do responsável: ')
+colab = input('Digite o nome do funcionário: ')
+event = input('Digite o nome do evento: ')
+amount = input('Digite o valor que será ressarcido: ')
+
+print('Declaro para o senhor ' + person_in_charge + ', que o senhor ' + colab + ' esteve presente no evento ' + event + ' e gastou o valor de R$ ' + str(amount) + ' com a entrada.')
