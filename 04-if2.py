2a8e2e07bf2a0949ee8753a8566d5d33287b1e7b
dressaco <>
2021-04-26 12:47:08 +0000

    update if2

--- 04-if2.py
+++ 04-if2.py
@@ -1,10 +1,10 @@
 name = input('Digite o nome: ')
 age = int(input('Digite a idade: '))
-contagious_susp = input('Suspeita de doença infecto-contagiosa?').upper()
+contagious_susp = input('Suspeita de doença infecto-contagiosa? (S/N)').upper()
 
 if age >= 65:
   print('O paciente ' + name + ' possui atendimento prioritário!')
-elif contagious_susp == 'SIM':
+elif contagious_susp == 'S':
   print('O paciente ' + name + ' deve ser direcionado para sala de espera reservada.')
 else:
   print('O paciente ' + name + ' NÃO possui atendimento prioritário e pode aguardar na sala comum!')
