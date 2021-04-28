1794f4fc0adb63979f6e43386fb030886fd24567
Andressa Collin <dressaco@outlook.com>
2021-04-28 04:14:30 +0000

    create multiplas_listas

---
+++ 08-multiplas_listas.py
@@ -0,0 +1,21 @@
+equipamentos = []
+valores = []
+seriais = []
+departamentos = []
+resposta = 'S'
+
+while resposta == 'S':
+  equipamentos.append(input('Equipamento: '))
+  valores.append(float(input('Valor: ')))
+  seriais.append(int(input('Número Serial: ')))
+  departamentos.append(input('Departamento: '))
+  
+  resposta = input('Digite \"S\" para continuar: ').upper()
+  #.upper -> força o input a ficar com letras maiusculas
+
+  for indice in range(0,len(equipamentos)):
+    print('\nEquipamento..:' , (indice+1))
+    print('Nome.........:' , equipamentos[indice])
+    print('Valor........:' , valores[indice])
+    print('Serial.......:' , seriais[indice])
+    print('Departamento.:' , departamentos[indice])
