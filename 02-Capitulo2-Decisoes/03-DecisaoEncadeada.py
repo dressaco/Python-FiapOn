9b5228906ecd9a73c1192c8a4224cbe177ee1b9a
Andressa Collin <dressaco@outlook.com>
2021-05-01 22:07:27 +0000

    recreate previous exercise with chain decision structure

---
+++ 02-Capitulo2-Decisoes/03-DecisaoEncadeada.py
@@ -0,0 +1,23 @@
+name = input('Digite o nome: ')
+age = int(input('Digite a idade: '))
+contagious_susp = input('Suspeita de doença infecto-contagiosa (S/N)?').upper()
+
+if age >= 65:
+  print('Paciente COM prioridade')
+  if contagious_susp == 'S':
+  print('Encaminhe o paciente para a sala AMARELA')
+  elif contagious_susp == 'N':
+  print('Encaminhe o paciente para a sala BRANCA')
+  else:
+  print('Responda a suspeita de doença infectocontagiosa com \'S\' ou \'N\'')
+
+if age < 65:
+  print('Paciente SEM prioridade')
+  if contagious_susp == 'S':
+  print('Encaminhe o paciente para a sala AMARELA')
+  elif contagious_susp == 'N':
+  print('Encaminhe o paciente para a sala BRANCA')
+  else:
+  print('Responda a suspeita de doença infectocontagiosa com \'S\' ou \'N\'')
+
+print('FIM')
