4ff9764c088a513b818b3b21381ea7863c2d7205
Andressa Collin <dressaco@outlook.com>
2021-05-12 04:15:16 +0000

    create DesafioDecisao

---
+++ 02-Capitulo2-Decisoes/05-DesafioDecisao.py
@@ -0,0 +1,16 @@
+role = input('Digite o nível de acesso: ').upper()
+
+if role == 'ADM' or role == 'USR':
+  gender = input('Digite o seu gênero: ').upper()
+  if role == 'ADM' and gender == 'F':
+    print('Olá administradora')
+  elif role == 'ADM' and gender == 'M':
+    print('Olá administrador')
+  elif role == 'USR' and gender == 'F':
+    print('Olá usuária')
+  elif role == 'USR' and gender == 'M':
+    print('Olá usuário')
+elif role == 'GUEST':
+  print('Olá visitante')
+else:
+  print('Olá desconhecido(a)')
