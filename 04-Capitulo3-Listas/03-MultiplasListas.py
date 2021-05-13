2478a5e24552c4c3e92bf3cba4dbc7c2c43be895
Andressa Collin <dressaco@outlook.com>
2021-05-13 00:13:15 +0000

    update MultiplasListas

--- 04-Capitulo3-Listas/03-MultiplasListas.py
+++ 04-Capitulo3-Listas/03-MultiplasListas.py
@@ -13,9 +13,18 @@ while resposta == 'S':
   resposta = input('Digite \"S\" para continuar: ').upper()
   #.upper -> força o input a ficar com letras maiusculas
 
+'''
   for indice in range(0,len(equipamentos)):
     print('\nEquipamento..:' , (indice+1))
     print('Nome.........:' , equipamentos[indice])
     print('Valor........:' , valores[indice])
     print('Serial.......:' , seriais[indice])
     print('Departamento.:' , departamentos[indice])
+'''
+#um pouco mais sobre listas
+search = input('Digite o nome do equipamento que deseja buscar: ')
+
+for indice in range (0,len(equipamentos)):
+  if search == equipamentos[indice]:
+    print('Valor........:' , valores[indice])
+    print('Serial.......:' , seriais[indice])
